d50add75f571073180c76d4c5fdfa8296b212bc5
Po-Jan <jan.poljsak@tscng.org>
2024-11-19 20:46:36 +0100

    Add files via upload

--- javascript/submitLogin.js
+++ javascript/submitLogin.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     function validateEmail(email) {
-        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         return emailRegex.test(email);
     }
 
