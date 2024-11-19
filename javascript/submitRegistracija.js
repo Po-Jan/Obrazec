bd662b79df41a59d17c839daef8fda24bd010cff
Po-Jan <jan.poljsak@tscng.org>
2024-11-19 20:49:41 +0100

    Add files via upload

--- javascript/submitRegistracija.js
+++ javascript/submitRegistracija.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
         return passRegex.test(password.value.trim());
     }
     function validateEmail(email) {
-        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         return emailRegex.test(email);
     }
     function validateUsername() {
