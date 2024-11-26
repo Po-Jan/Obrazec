54d7b4c9c78cabedb1222c6dd6c36e122d7d3aa8
Po-Jan <jan.poljsak@tscng.org>
2024-11-26 21:09:33 +0100

    Add files via upload

--- javascript/vizitka.js
+++ javascript/vizitka.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const face= document.querySelector('.face');
-    face.addEventListener('click', function() {
+    const innerFace= document.querySelector('.innerFace');
+   innerFace.addEventListener('click', function() {
         swal("Credits:", "\nJan Poljšak, 4.Ra \nproject made from HTML, CSS and JavaScript\n Teacher: Boštjan Vouk", "info");
 
         });
