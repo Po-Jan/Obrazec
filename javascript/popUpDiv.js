e2a76198479f0aedcc6a2208b622ff8ce21601c4
Po-Jan <jan.poljsak@tscng.org>
2024-12-02 19:13:35 +0100

    Add files via upload

--- javascript/popUpDiv.js
+++ javascript/popUpDiv.js
@@ -2,8 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const displayName= document.querySelector('#displayName');
     const textDisplayName=document.querySelector('.textDisplayName');
     
+    displayName.addEventListener("mouseenter", () => {
+        mouseHovered = true; 
+    });
+    
 
-    displayName.addEventListener('click',function(){
+    displayName.addEventListener("mouseleave", () => {
+        mouseHovered = false; 
+    });
+
+    displayName.addEventListener('click',function(event){
         event.stopPropagation();
         textDisplayName.classList.add('displayBlock'); 
         textDisplayName.classList.add('expand');
@@ -11,28 +19,69 @@ document.addEventListener('DOMContentLoaded', function() {
         textUsername.classList.remove('expand'); 
     });
 
+    displayName.addEventListener('focusin',function(event){
+        event.stopPropagation();
+        if(!mouseHovered){
+            textDisplayName.classList.add('displayBlock'); 
+            textDisplayName.classList.add('expand');
+            textUsername.classList.remove('displayBlock');
+            textUsername.classList.remove('expand');
+        }
+ 
+    });
 
 
     const username=document.querySelector('#username');
     const textUsername=document.querySelector('.textUsername');
 
-    username.addEventListener('click',function(){
+    username.addEventListener("mouseenter", () => {
+        mouseHovered1 = true; 
+    });
+    
+
+    username.addEventListener("mouseleave", () => {
+        mouseHovered1 = false; 
+    });
+
+    username.addEventListener('click',function(event){
         event.stopPropagation();
-        textUsername.classList.add('displayBlock'); 
-        textUsername.classList.add('expand'); 
-        textDisplayName.classList.remove('displayBlock');
-        textDisplayName.classList.remove('expand'); 
+            textUsername.classList.add('displayBlock'); 
+            textUsername.classList.add('expand'); 
+            textDisplayName.classList.remove('displayBlock');
+            textDisplayName.classList.remove('expand'); 
         
     });
 
+    username.addEventListener('focusin',function(event){
+        event.stopPropagation();
+        if(!mouseHovered1){
+            textUsername.classList.add('displayBlock'); 
+            textUsername.classList.add('expand'); 
+            textDisplayName.classList.remove('displayBlock');
+            textDisplayName.classList.remove('expand'); 
+        }
+    });
+    username.addEventListener('focusout',function(event){
+        event.stopPropagation();
+        if(!mouseHovered1){
+            textUsername.classList.remove('displayBlock'); 
+            textUsername.classList.remove('expand'); 
+            textDisplayName.classList.remove('displayBlock');
+            textDisplayName.classList.remove('expand'); 
+        }
+    });
 
 
-    document.addEventListener('click',function(){
+    
+
+    document.addEventListener('click',function(event){
         textUsername.classList.remove('displayBlock');
         textUsername.classList.remove('expand'); 
         textDisplayName.classList.remove('displayBlock');
         textDisplayName.classList.remove('expand');
     });
+
+
 
 
 });
